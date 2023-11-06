0b74858e37206b13e15478e76f0b0c6c7058ea80
Coffee-0 <mailhemanth25@gmail.com>
2023-11-07 01:38:45 +0530

    connected to mongodb database

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 
 // routes
@@ -6,6 +7,21 @@ app.get("/", (req, res) => {
   res.send("Hello Node API");
 });
 
+app.get("/blog", (req, res) => {
+  res.send("hello blog");
+});
+
 app.listen(3000, () => {
   console.log("node api app is running on port 3000");
 });
+
+mongoose
+  .connect(
+    "mongodb+srv://admin:<password>@express-api.n7ykngh.mongodb.net/Express-API?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    console.log("connected to Mongo DB");
+  })
+  .catch((error) => {
+    console.log("cannot connect to database \n", error);
+  });
