7f695fdcf7cffdeb31c3c761db96d91ff3e0e5a7
Coffee-0 <mailhemanth25@gmail.com>
2023-11-08 00:40:09 +0530

    add product data model

--- server.js
+++ server.js
@@ -11,16 +11,20 @@ app.get("/blog", (req, res) => {
   res.send("hello blog");
 });
 
-app.listen(3000, () => {
-  console.log("node api app is running on port 3000");
-});
+// app.listen(3000, () => {
+//   console.log("node api app is running on port 3000");
+// });
 
+mongoose.set("strictQuery", false);
 mongoose
   .connect(
     "mongodb+srv://admin:<password>@express-api.n7ykngh.mongodb.net/Express-API?retryWrites=true&w=majority"
   )
   .then(() => {
     console.log("connected to Mongo DB");
+    app.listen(3000, () => {
+      console.log("node api app is running on port 3000");
+    });
   })
   .catch((error) => {
     console.log("cannot connect to database \n", error);
